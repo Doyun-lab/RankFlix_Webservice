d79f19a36ad1d656dc3335fb759a57a6d75c1947
Ubuntu <ubuntu@ip-172-31-8-83.ap-northeast-2.compute.internal>
2021-05-30 13:16:06 +0000

    service adjust

--- service/test.sh
+++ service/test.sh
@@ -1,14 +1,10 @@
 if [ $# -lt 1 ]; then
-        echo 'give at least 1 argument'
-Else
+        echo 'give 1 argument'
+else
         if [ $1 = 'login' ]; then
-                SESSION=$(curl -H "Content-type: application/json" -X POST -d '{"user_id":"iam","passwd":"biggong"}' http://127.0.0.1:11100/login)
-                echo "$SESSION"
-                echo "$SESSION" >> ret
-        elif [ $1 = 'fav-add' ]; then
-                statement='{"request_type":"add","session_id":"'$2'","favorite":["킹덤", "옥자"]}'
-                echo "$statement"
-                curl -H "Content-type: application/json" -X POST -d "$statement" http://127.0.0.1:11100/favorite >> ret
+                curl -H "Content-type: application/json" -X POST -d '{"user_id":"iam","passwd":"biggong"}' http://127.0.0.1:11100/login >> ret
+        elif [ $1 = 'favorite' ]; then
+                curl -H "Content-type: application/json" -X POST -d '{"session_id":"xxxxxxxx"}' http://127.0.0.1:11100/favorite >> ret
         fi
 fi
 
